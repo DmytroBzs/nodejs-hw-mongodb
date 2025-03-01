422022644bffceb5381aaf2cb065790b93548c7e
DmytroBzs <godimmortalis@gmail.com>
2025-03-01 20:28:11 +0200

    cloudinary_add

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
-
+  app.use(router);
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
@@ -33,8 +33,6 @@ export const startServer = () => {
   });
   app.use('/uploads', express.static(UPLOAD_DIR));
   app.use('/api-docs', swaggerDocs());
-
-  app.use(router);
 
   app.use('*', notFoundHandler);
 
